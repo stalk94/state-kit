c6274ba5f9870e819f20f80846164d2fd2efce78
pigson <stalk9424@gmail.com>
2025-05-24 03:12:53 +0200

    modified:   README.md 	modified:   src/example/App.tsx

--- README.md
+++ README.md
@@ -47,44 +47,44 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createState, store, useLocalStorage } from 'statekit-react';
 
+// create slice state
 const counter = createState('counter', { value: 0 });
 
 // Pure watcher (not bound to any DOM)
 function Logger() {
-  useEffect(() => {
-    const unsub = counter.value.useWatch((v) => console.log('[useWatch] value changed:', v));
-    return unsub;
-  }, []);
+    counter.value.useWatch((v) => {
+        console.log('[useWatch] value changed:', v)
+    });
 
-  return null;
+    return null;
 }
 
 // UI component that reacts to changes
 function Display() {
-  const value = counter.value.use();
-  return <div>Value: {value}</div>;   // or even easier: <div>Value: {counter.value.use()}</div>
+    const value = counter.value.use();
+    return <div>Value: {value}</div>;   // or even easier: <div>Value: {counter.value.use()}</div>
 }
 
 // Updates state every second
 function Updater() {
-  useEffect(() => {
-    const i = setInterval(() => {
-      counter.value.set(v => v + 1);
-    }, 1000);
-    return () => clearInterval(i);
-  }, []);
-  return null;
+    React.useEffect(() => {
+        const i = setInterval(() => {
+            counter.value.set(v => v + 1);
+        }, 1000);
+        return () => clearInterval(i);
+    }, []);
+    return null;
 }
 
 // Root component
 export function App() {
-  return (
-    <Provider store={store}>
-      <Logger />
-      <Updater />
-      <Display />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Logger />
+            <Updater />
+            <Display />
+        </Provider>
+    );
 }
 
 ```
--- src/example/App.tsx
+++ src/example/App.tsx
@@ -4,64 +4,43 @@ import { createState, store, useLocalStorage } from '../index';
 import { createRoot } from 'react-dom/client';
 
 
-const editor = createState('editor', {
-    layout: [],
-    mod: 'block',
-    size: { width: 100, height: 200 },
-}, [
-    useLocalStorage({ restore: true })
-]);
+const counter = createState('counter', { value: 0 });
 
+// Pure watcher (not bound to any DOM)
+function Logger() {
+    counter.value.useWatch((v) => {
+        console.log('[useWatch] value changed:', v)
+    });
 
-
-function Test2() {
-    const size = editor.size.use();
-    
-
-    return(
-        <div style={{marginTop: '20px', color: 'red'}}>
-            {/* or: editor?.size?.width.use() */}
-            { size.width }                   
-        </div>
-    );
+    return null;
 }
 
-function Test({ }) {
-    const size = editor.size.use();
-    
-   
+// UI component that reacts to changes
+function Display() {
+    const value = counter.value.use();
+    return <div>Value: {value}</div>;   // or even easier: <div>Value: {counter.value.use()}</div>
+}
 
+// Updates state every second
+function Updater() {
     React.useEffect(() => {
         const i = setInterval(() => {
-             editor.size.set((prev)=> {
-                prev.width = prev.width + 1
-                prev.height++
-                return prev;
-             })
+            counter.value.set(v => v + 1);
         }, 1000);
-
         return () => clearInterval(i);
     }, []);
-
-
-    return(
-        <div>
-            { size?.width }
-            <Test2 />
-        </div>
-    );
+    return null;
 }
 
-
-function App() {
+// Root component
+export function App() {
     return (
         <Provider store={store}>
-            <div style={{ marginTop: '10%', marginLeft: '20%' }}>
-                <Test />
-            </div>
+            <Logger />
+            <Updater />
+            <Display />
         </Provider>
     );
 }
-
 
 createRoot(document.querySelector(".root")).render(<App/>);
